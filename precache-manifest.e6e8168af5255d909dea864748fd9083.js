a2a98c154afbbcfad7461d9f8060c4bea7883966
Nazmul <sisil8sisil@gmail.com>
2019-03-10 02:24:32 +0600

    Updates

---
+++ precache-manifest.e6e8168af5255d909dea864748fd9083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-defaults/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "70cb1343221ada0818fe",
+    "url": "/react-defaults/static/js/runtime~main.bf697f7b.js"
+  },
+  {
+    "revision": "9410d4b926ca98aff0c8",
+    "url": "/react-defaults/static/js/main.ef928490.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/react-defaults/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "9410d4b926ca98aff0c8",
+    "url": "/react-defaults/static/css/main.390993d9.chunk.css"
+  },
+  {
+    "revision": "861c2c20de3293834d823b506cfee915",
+    "url": "/react-defaults/index.html"
+  }
+];
